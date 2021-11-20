66fa0d34085995371ff4dfe449566f24e61ca33b
Yang Yang <yyang@streamnative.io>
2021-11-20 11:58:10 +0800

    Support revoking permissions.

--- src/admin/namespaces.rs
+++ src/admin/namespaces.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::error::Error;
 
 use crate::admin::admin::PulsarAdmin;
@@ -7,9 +8,47 @@ pub struct PulsarAdminNamespaces {
 }
 
 impl PulsarAdminNamespaces {
-    pub async fn list(&self, tenant: &str) -> Result<Vec<String>, Box<dyn Error>> {
-        Ok(self.admin.get(format!("/admin/v2/namespaces/{}", tenant).as_str())?
+    pub async fn list(&self, namespace: &str) -> Result<Vec<String>, Box<dyn Error>> {
+        Ok(self.admin.get(format!("/admin/v2/namespaces/{}", namespace).as_str())?
             .send().await?
             .json::<Vec<String>>().await?)
+    }
+
+    pub async fn permissions(&self, namespace: &str) -> Result<HashMap<String, Vec<String>>, Box<dyn Error>> {
+        let res = self.admin.get(format!("/admin/v2/namespaces/{}/permissions", namespace).as_str())?
+            .send().await?;
+        let body = res.text().await?;
+        info!("{}", body.as_str());
+
+        Ok(serde_json::from_str::<HashMap<String, Vec<Option<String>>>>(body.as_str())?.into_iter()
+            .map(|e| {
+                let values: Vec<String> = e.1.into_iter()
+                    .filter(|op| op.is_some())
+                    .map(|op| op.unwrap())
+                    .collect();
+                (e.0, values)
+            })
+            .collect())
+    }
+
+    pub async fn grant_permission(&self, namespace: &str, role: &str, permissions: &Vec<String>) -> Result<(), Box<dyn Error>> {
+        let res = self.admin.post(format!("/admin/v2/namespaces/{}/permissions/{}", namespace, role).as_str())?
+            .json(permissions)
+            .send().await?;
+        if res.status().is_success() {
+            Ok(())
+        } else {
+            Err(Box::from(res.text().await?))
+        }
+    }
+
+    pub async fn revoke_permission(&self, namespace: &str, role: &str) -> Result<(), Box<dyn Error>> {
+        let res = self.admin.delete(format!("/admin/v2/namespaces/{}/permissions/{}", namespace, role).as_str())?
+            .send().await?;
+        if res.status().is_success() {
+            Ok(())
+        } else {
+            Err(Box::from(res.text().await?))
+        }
     }
 }
--- src/cmd/namespaces.rs
+++ src/cmd/namespaces.rs
@@ -14,6 +14,9 @@ impl NamespacesOpts {
     pub async fn run(&self, pulsar_ctx: &mut PulsarContext) -> Result<(), Box<dyn Error>> {
         match &self.cmd {
             Command::List(opts) => opts.run(pulsar_ctx).await?,
+            Command::Permissions(opts) => opts.run(pulsar_ctx).await?,
+            Command::GrantPermission(opts) => opts.run(pulsar_ctx).await?,
+            Command::RevokePermission(opts) => opts.run(pulsar_ctx).await?,
         }
         Ok(())
     }
@@ -22,6 +25,9 @@ impl NamespacesOpts {
 #[derive(Clap, Debug, Clone)]
 pub enum Command {
     List(ListOpts),
+    Permissions(PermissionsOpts),
+    GrantPermission(GrantPermissionOpts),
+    RevokePermission(RevokePermissionOpts),
 }
 
 #[derive(Clap, Debug, Clone)]
@@ -34,6 +40,63 @@ impl ListOpts {
         let r = pulsar_ctx.admin().await?
             .namespaces()
             .list(self.tenant.as_str())
+            .await?;
+        println!("{:?}", r);
+        Ok(())
+    }
+}
+
+#[derive(Clap, Debug, Clone)]
+pub struct PermissionsOpts {
+    namespace: String,
+}
+
+impl PermissionsOpts {
+    pub async fn run(&self, pulsar_ctx: &mut PulsarContext) -> Result<(), Box<dyn Error>> {
+        let r = pulsar_ctx.admin().await?
+            .namespaces()
+            .permissions(self.namespace.as_str())
+            .await?;
+        println!("{:?}", r);
+        Ok(())
+    }
+}
+
+#[derive(Clap, Debug, Clone)]
+pub struct GrantPermissionOpts {
+    namespace: String,
+
+    #[clap(long)]
+    role: String,
+
+    #[clap(long)]
+    actions: Vec<String>,
+}
+
+impl GrantPermissionOpts {
+    pub async fn run(&self, pulsar_ctx: &mut PulsarContext) -> Result<(), Box<dyn Error>> {
+        let r = pulsar_ctx.admin().await?
+            .namespaces()
+            .grant_permission(self.namespace.as_str(), self.role.as_str(), &self.actions)
+            .await?;
+        println!("{:?}", r);
+        Ok(())
+    }
+}
+
+#[derive(Clap, Debug, Clone)]
+pub struct RevokePermissionOpts {
+    namespace: String,
+
+    #[clap(long)]
+    role: String,
+}
+
+impl RevokePermissionOpts {
+    pub async fn run(&self, pulsar_ctx: &mut PulsarContext) -> Result<(), Box<dyn Error>> {
+        let r = pulsar_ctx.admin().await?
+            .namespaces()
+            .revoke_permission(self.namespace.as_str(), self.role.as_str())
             .await?;
         println!("{:?}", r);
         Ok(())
